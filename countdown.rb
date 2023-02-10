c66fe1d9ac225b468d9d70a5787b80708c4f63c5
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-02-10 18:40:06 +0300

    commit tree ecdea55a889a4b950b1c15459f56601e6ad4b969

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    
+    while(number>0)
+       puts "#{number.to_s} SECOND(S)!"
+        number-=1
+    end
+        "HAPPY NEW YEAR!"
+    
+
 end
+
+def countdown_with_sleep(count)
+    count.downto(1) do |i|
+      puts i
+      sleep(1)
+    end
+  end
+  
